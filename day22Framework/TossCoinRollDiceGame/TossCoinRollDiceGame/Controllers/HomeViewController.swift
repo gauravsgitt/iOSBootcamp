2b267b8dc83ee876bd92a22044f4e437febe3cc0
= <=>
2021-11-16 09:48:54 +0530

    TTN assignment 22 added.

---
+++ day22Framework/TossCoinRollDiceGame/TossCoinRollDiceGame/Controllers/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TossCoinRollDiceGame
+//
+//  Created by Gaurav Bisht on 15/11/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
---
+++ day22Framework/TossCoinRollDiceGame/TossCoinRollDiceGame/Utility.swift
@@ -0,0 +1,10 @@
+//
+//  Utility.swift
+//  TossCoinRollDiceGame
+//
+//  Created by Gaurav Bisht on 15/11/21.
+//
+
+import Foundation
+
+
