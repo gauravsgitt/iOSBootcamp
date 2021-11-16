1a15c44d4b8982ef444bc9bdfe57457b6c4adca5
= <=>
2021-11-16 10:19:39 +0530

    TTN assignment 23 and 25 added.

---
+++ day23DataPersistence/iOSAssessment-23/iOSAssessment-23/Controller/FoodTabViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FoodTabViewController.swift
+//  iOSAssessment-23
+//
+//  Created by Gaurav Bisht on 08/11/21.
+//
+
+import UIKit
+
+class FoodTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
